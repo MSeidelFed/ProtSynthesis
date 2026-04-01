23132cf38ae4b5a89db8a87f5ca067b91bf71dd6
Ethan C Strayer <ethancstrayer@gmail.com>
2026-04-01 13:55:31 -0600

    Updated CLI

--- python/src/ProtSythesis/run_isocorr.py
+++ python/src/ProtSythesis/run_isocorr.py
@@ -5,10 +5,11 @@ import pandas as pd
 import numpy as np
 
 import isocor
+import count_glyser
 
 
 
-def build_isotope_lookup():
+def build_isotope_lookup(isotopes_path):
 
     '''
     Function to generated isotope dictionary from isotope.dat file
@@ -18,7 +19,7 @@ def build_isotope_lookup():
 
     isotopes = {}
 
-    with open("Isotopes.dat", 'r') as f:
+    with open(isotopes_path, 'r') as f:
         for i,l in enumerate(f.readlines()):
             l = l.strip('\n')
             if i == 0:
@@ -57,7 +58,7 @@ def consolidate_data(file_list):
     return df
 
 
-def make_inputs_to_isocorr(df, tracer_mass = 15, tracer_element = "N", tracer_purity = [0.01,0.99]):
+def make_inputs_to_isocorr(df, isotopes, tracer_mass = 15, tracer_element = "N", tracer_purity = [0.01,0.99]):
     '''
     Parses raw dataframe to run isocorr
     returns dataframe containing the following columns [sample, protein_key, protein_group, mean_enrichment]
@@ -65,7 +66,6 @@ def make_inputs_to_isocorr(df, tracer_mass = 15, tracer_element = "N", tracer_pu
 
     tracer = f'{tracer_mass}{tracer_element}'
 
-    isotopes = build_isotope_lookup()
     metabolties = {}
     measurements = []
 
@@ -94,10 +94,8 @@ def make_inputs_to_isocorr(df, tracer_mass = 15, tracer_element = "N", tracer_pu
         _,_,_, menrich = metabolties[peptide].correct(peaks)
         mean_enrichment.append(menrich)
 
-    df_isocorr = df.loc[:,['sample','peptide_key','protein_gorup']]
+    df_isocorr = df.loc[:,['file','peptide_key','protein_gorup']]
     
-    df_isocorr.columns = ['sample','peptide_key', 'protein_group']
-
     df_isocorr.loc[:,'mean_enrichment'] = mean_enrichment
     
     return df_isocorr
@@ -107,15 +105,38 @@ def make_inputs_to_isocorr(df, tracer_mass = 15, tracer_element = "N", tracer_pu
 def main():
 
     parser = argparse.ArgumentParser(description= "Python wrapper around isocor")
-    parser.add_argument("-f", type= str, help="Path to files with wild card")
+    parser.add_argument("-f", type= str, help="Path to results.dat")
+    parser.add_argument("-i", type = str, help='isotopes.dat path')
     parser.add_argument("-o", type = str, help="Output path")
+    parser.add_argument("-s", type=str, help = "path to schema" )
     args = parser.parse_args()
 
-    file_list = glob.glob(args.f)
+    results_path = args.f
+    raw_data = pd.read_csv(results_path, sep = '\t')
+    raw_data.columns = [c.replace(' ','_').lower() for c in raw_data.columns]
+    raw_data = raw_data.fillna(0)
 
-    raw_data = consolidate_data(file_list = file_list)
-    isocorrected_data = make_inputs_to_isocorr(raw_data)
-    isocorrected_data.to_csv('output/isocorrected_data.csv')
+    output_path = args.o
+    isotopes_path = args.i
+    schema_path = args.s
+
+    schema_path = 'data/total_proteome_labeled/2026_schema_shoot.csv'
+    schema_df = pd.read_csv(schema_path)
+
+    sample_lookup = {file:f'{sample}_{file.split('_')[-1]}' for _, sample, file in schema_df.loc[:,['Sample','File']].itertuples()}
+    
+
+    isotopes = build_isotope_lookup(isotopes_path)
+
+    isocorrected_data = make_inputs_to_isocorr(raw_data, isotopes= isotopes)
+    isocorrected_data.loc[:,'sample'] = isocorrected_data.loc[:,'file'].map(sample_lookup)
+    #isocorrected_data = isocorrected_data[isocorrected_data.loc[:,"mean_enrichment"] != 0]
+
+    isocorrected_data.loc[:,'gly_ser_count'] = isocorrected_data.loc[:,'peptide_key'].apply(count_glyser.count_gly_ser)
+    #isocorrected_data = isocorrected_data[isocorrected_data.loc[:,"gly_ser_count"] != 0]
+
+    isocorrected_data.loc[:,'normalized_mean_enrichment'] = (isocorrected_data.loc[:,'mean_enrichment'] / isocorrected_data.loc[:,'gly_ser_count'].replace(0,np.nan)).replace(np.nan, 0)
+    isocorrected_data.to_csv(output_path)
 
 
 if __name__ == "__main__":
